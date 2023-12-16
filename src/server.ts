6dae1076572126add72b20c7c0c3bc75a8c5496a
bibhashhalder <halderbibhash80@gmail.com>
2023-12-16 20:44:32 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+
+import app from "./app";
+import mongoose from 'mongoose';
+import config from "./app/config";
+
+
+async function main() {
+    try{
+        await mongoose.connect(config.database_url as string);
+  
+    app.listen(config.port,()=>{
+        console.log(`This server is running on port ${config.port}`)
+    })
+    }catch(err){
+        console.log(err)
+    }
+  }
+
+main()
